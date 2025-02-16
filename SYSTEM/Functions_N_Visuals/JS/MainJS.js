8c6628db0c9f5a30d042d9638fc43ce4e676f70e
EJDelRosario24 <138583463+EJDelRosario24@users.noreply.github.com>
2025-02-17 01:23:31 +0800

    Add files via upload

---
+++ SYSTEM/Functions_N_Visuals/JS/MainJS.js
@@ -0,0 +1,80 @@
+
+var MainButton = document.getElementById("Btn_TorF");
+var WPC = document.getElementById("TheBox");
+var XBtn = document.getElementById("CloseBtn");
+
+
+MainButton.addEventListener("click", function(){
+
+  if (MainButton.classList.contains("MainBtnAnimation")) return;
+
+       MainButton.classList.add("MainBtnAnimation");
+
+MainButton.addEventListener("animationend", function() {
+      MainButton.classList.remove("MainBtnAnimation");
+      WPC.style.display = "block";
+      MainButton.style.display = "none";
+
+  MainButton.removeEventListener("animationend", handler);
+   });
+   if (WPC.style.display==="block"){
+        woosh.currentTime = 0;
+        woosh.play();
+   } else {
+        woosh.pause();   
+   }
+}); 
+
+
+XBtn.addEventListener("click", function() {
+  
+    WPC.style.display = "none";
+    MainButton.style.display ="block" ;
+  
+  });
+
+
+
+
+
+  function SelectedPage1(){
+    window.location.href = "HTMLs/BasicCalculator/Basic_Calculator.html";
+     
+  }
+
+  let buttons = document.getElementsByTagName("button");
+  let audio = document.getElementById("BtnSound");
+  
+  for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", function () {
+          audio.currentTime = 0; 
+          audio.play();
+      });
+      
+  }
+
+
+  var MainButton = document.getElementById("Btn_TorF");
+  let MBtnSound = document.getElementById("BtnSound1");
+
+
+  MainButton.addEventListener("click", function () {
+    MBtnSound.currentTime = 0; 
+    MBtnSound.play();
+});
+
+
+/*
+var SelectionButtons =  document.querySelector(".SelectionButtons");
+   var BtnFlashAnm = documents.querySelector(".BtnFlash");
+
+SelectionButtons.addEventListener("mouseenter", ()=>{
+
+  BtnFlashAnm.classList.add("BtnFlashAnimation");
+});
+
+SelectionButtons.addEventListener("mouseleave", ()=>{
+
+  BtnFlashAnm.classList.remove("BtnFlashAnimation");
+  });
+  */
